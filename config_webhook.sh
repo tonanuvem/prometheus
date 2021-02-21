21487e1ec3a78ef52e05828afa8302603533e17d
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-21 17:11:12 -0300

    Update config_webhook.sh

--- config_webhook.sh
+++ config_webhook.sh
@@ -3,3 +3,8 @@ read WEBHOOK
 
 sed -i 's|inserir_webhook|'$WEBHOOK'|' alertmanager/config.yml
 # sed -i 's|lab-monitoracao|lab-testes|' alertmanager/config.yml
+
+echo "Digite seu NOME:"
+read NOME
+
+sed -i 's|Prometheus|'$NOME'_Prometheus|' alertmanager/config.yml
