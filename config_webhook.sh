f2dd0d5f3bfcead6531a5e03907417199ad082bf
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-21 16:44:23 -0300

    Update config_webhook.sh

--- config_webhook.sh
+++ config_webhook.sh
@@ -2,3 +2,4 @@ echo "Digite a URL que está no canal #webhook do SLACK:"
 read WEBHOOK
 
 sed -i 's|inserir_webhook|'$WEBHOOK'|' alertmanager/config.yml
+# sed -i 's|lab-monitoracao|lab-testes|' alertmanager/config.yml
