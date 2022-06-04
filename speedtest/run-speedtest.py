d3dcabc9f4f589bb5c37f52ebba0ff06298ad6b4
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-06-03 21:11:57 -0300

    Update run-speedtest.py

--- speedtest/run-speedtest.py
+++ speedtest/run-speedtest.py
@@ -5,6 +5,12 @@ import time
 from prometheus_client import start_http_server, Summary, Gauge
 import logging
 
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+
 servers = []
 # If you want to test against a specific server
 # servers = [1234]
@@ -19,8 +25,11 @@ g_upload = Gauge('upload_speed', 'Upload speed')
 def process_request(t):
   s.get_servers(servers)
   s.get_best_server()
+  logging.warning("\tAplicacao Speed Test selecionou SERVIDORES para efetuar os testes de velocidade")
   s.download()
+  logging.warning("\tAplicacao Speed Test fez testes de velocidade de DOWNLOAD")
   s.upload()
+  logging.warning("\tAplicacao Speed Test fez testes de velocidade de UPLOAD")
   results_dict = s.results.dict()
   g_download.set(results_dict["download"])
   g_upload.set(results_dict["upload"])
@@ -30,7 +39,7 @@ def process_request(t):
   
 if __name__ == '__main__':
   # Start up the server to expose the metrics.
-  logging.warning("Aplicacao Speed Test foi iniciada")
+  logging.warning("\tAplicacao Speed Test foi iniciada")
   start_http_server(9104) 
   # Generate some requests.
   while True:
