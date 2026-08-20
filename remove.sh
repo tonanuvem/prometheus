70b9627caa401aa20ec4a88422cfb67bfdd79963
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2026-08-20 16:02:35 -0300

    Update remove.sh to use 'docker compose' command

---
+++ remove.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker compose stop && docker compose rm -f && docker volume prune -f && git pull
+echo ""
+echo "Removendo volumes:"
+echo ""
+# Lista todos os volumes e remove cada um
+docker volume ls -q | grep prometheus | while read volume; do
+  #echo "Removendo volume: $volume"
+  docker volume rm "$volume"
+done
